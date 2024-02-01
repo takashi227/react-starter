4b4d1595bb78efa05575ab62deb279116cd60f52
takashi <pero857942632@gmail.com>
2024-02-01 22:18:25 +0900

    list3-25を実装

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import React, { Component } from 'react';
-import Rect from './Rect';
+import { Component } from 'react';
+import Message from './component/Message';
 
 class App extends Component {
 
@@ -9,10 +9,11 @@ class App extends Component {
       <div>
         <h1 className="bg-primary text-white display-4">React</h1>
         <div className="container">
-          <p className="subtitle">draw rectangle.</p>
-          <Rect x="200" y="200" width="200" height="200" color="#6ff9" radius="25" />
-          <Rect x="300" y="300" width="200" height="200" color="#f6f9" radius="75" />
-          <Rect x="400" y="400" width="200" height="200" color="#6669" radius="100" />
+          <Message title="Children!">
+            これはコンポーネント内のコンテンツです。
+            丸でテキストを分割し、リストにして表示します。
+            改行は必要ありません。
+          </Message>
         </div>
       </div>
     )
---
+++ src/component/Message.js
@@ -0,0 +1,31 @@
+import { Component } from "react";
+
+class Message extends Component {
+    li = {
+        fontSize:"14pt",
+        fontWeight:"bold",
+        color:"#090",
+    }
+
+    render() {
+        let content = this.props.children
+        let arr = content.split('。')
+        let arr2 = []
+        for(let i=0; i < arr.length; i++) {
+            if(arr[i].trim() != '') {
+                arr2.push(arr[i])
+            }
+        }
+        let list = arr2.map((value, key) => (
+            <li className="list-group-item" style={this.li} key={key}>{key + 1} . {value} .</li>
+        ))
+        return (
+            <div>
+                <h2>{this.props.title}</h2>
+                <ol className="list-group">{list}</ol>
+            </div>
+        )
+    }
+}
+
+export default Message
