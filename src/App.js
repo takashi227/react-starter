29fda0cb2169e10e3627184aea58267dc694e53e
takashi <pero857942632@gmail.com>
2024-02-06 22:18:56 +0900

    Providerを実装

--- src/App.js
+++ src/App.js
@@ -1,49 +1,16 @@
 import './App.css';
 import React, { Component } from 'react';
-
-let data = {
-  title: 'React-Context',
-  message: 'this is sample message.'
-}
-
-const SampleContext = React.createContext(data)
+import Provider from './component/Provider';
 
 class App extends Component {
 
   render() {
     return (
       <div>
-        <h1 className="bg-primary text-white display-4">React</h1>
-        <div className="container">
-          <Title />
-          <Message />
-        </div>
+        <Provider />
       </div>
     )
   }
 }
 
-class Title extends Component {
-  static contextType = SampleContext
-
-  render() {
-      return (
-          <div className="card p-2 my-3">
-              <h2>{this.context.title}</h2>
-          </div>
-      )
-  }
-}
-
-class Message extends Component {
-  static contextType = SampleContext
-
-  render() {
-      return (
-          <div className="alert alert-primary">
-              <p>{this.context.message}</p>
-          </div>
-      )
-  }
-}
 export default App;
