d945e12ca53e76eeeb425c7b99240adf02ac5ed4
takashi <pero857942632@gmail.com>
2024-02-06 22:53:12 +0900

    Themeを実装

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import React, { Component } from 'react';
 import Provider from './component/Provider';
+import Theme from './component/Theme';
 
 class App extends Component {
 
   render() {
     return (
       <div>
-        <Provider />
+        <Theme />
       </div>
     )
   }
---
+++ src/component/Theme.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+
+let theme = {
+    light:{
+        styles:{
+            backgroundColor:"#f0f9ff",
+            color:"#oof"
+        },
+        head: "bg-primary text-white display-4 mb-4",
+        alert: "alert alert-primary my-3",
+        text: "text-primary m-3",
+        foot: "py-4",
+    },
+    dark:{
+        styles:{
+            backgroundColor:"#336",
+            color:"#eef",
+        },
+        head: "bg-secondary text-white display-4 mb-4",
+        alert: "alert alert-dark my-3",
+        text: "text-light m-3",
+        foot: "py-4",
+    }
+}
+
+const ThemeContext = React.createContext(theme.light)
+
+class Theme extends Component {
+    static contextType = ThemeContext
+
+    render() {
+        return (
+            <div style={this.context.styles}>
+                <h1 className={this.context.head}>React</h1>
+                <div className="container">
+                    <Title value="Content page" />
+                    <Message value="This is Content sample." />
+                    <Message value="※これはテーマのサンプルです。" />
+                    <div className={this.context.foot}></div>
+                </div>
+            </div>
+        )
+    }
+}
+
+class Title extends Component {
+    static contextType = ThemeContext
+
+    render() {
+        return (
+            <div className={this.context.alert}>
+                <h2>{this.props.value}</h2>
+            </div>
+        )
+    }
+}
+
+class Message extends Component {
+    static contextType = ThemeContext
+
+    render() {
+        return (
+            <div>
+                <p className={this.context.text}>{this.props.value}</p>
+            </div>
+        )
+    }
+}
+
+export default Theme
