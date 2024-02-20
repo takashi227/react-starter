46867e2d094ebca9bd0a65eadb2a3ad5d244e6ee
takashi <pero857942632@gmail.com>
2024-02-20 23:34:39 +0900

    副作用フックを実装

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import React from 'react';
 import Form from './component-2/Form';
+import Effect from './component-2/Effect';
 
 function App() {
 
   return (
     <div>
-      <Form />
+      <Effect />
     </div>
   )
 }
---
+++ src/component-2/Effect.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react"
+
+function AlertMessage(props) {
+    return (
+        <div className="alert alert-primary h5 text-primary">
+            <h5>{props.msg}</h5>
+        </div>
+    )
+}
+
+function Effect() {
+    const [val, setVal] = useState(0)
+    const [msg, setMsg] = useState('set a number...')
+
+    const doChange = (event) => {
+        setVal(event.target.value)
+    }
+
+    useEffect(() => {
+        let total = 0
+        for(let i=0; i<=val; i++) {
+            total+=i
+        }
+        setMsg("total: " + total + ".")
+    })
+
+    return (
+        <div>
+            <h1 className="bg-primary text-white display-4">React</h1>
+            <div className="container">
+                <h4 className="my-3">Hooks sample</h4>
+                <AlertMessage msg={msg} />
+                <div className="form-group">
+                    <label>Input:</label>
+                    <input type="number" className="form-control" onChange={doChange} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Effect
